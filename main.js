e745be0215b3b5f62ec4167e90a0b0631451cb1f
Osvy1994 <osvaldo.elvirez@gmail.com>
2023-08-24 01:38:48 -0400

    prettier

--- main.js
+++ main.js
@@ -1,44 +1,43 @@
-let counter = 0
-const input = document.querySelector('input[type="text"]')
+let counter = 0;
+const input = document.querySelector('input[type="text"]');
 
-userInput.addEventListener('submit', () => {
-  addTask()
-})
-
+userInput.addEventListener("submit", () => {
+  addTask();
+});
 
 let addTask = () => {
   if (input.value) {
-  counter++
-  let newValue = input.value
-  list.innerHTML += `<div class="task-container" id="${counter}">
+    counter++;
+    let newValue = input.value;
+    list.innerHTML += `<div class="task-container" id="${counter}">
   <label> 
     <input type="checkbox" class="check">
     ${newValue}
   </label>
-  <img src="./images/delete.png" class="closeBtn"></div>`
+  <img src="./images/delete.png" class="closeBtn"></div>`;
 
-input.value = ""
-updateStats()
+    input.value = "";
+    updateStats();
   } else {
-    alert ('Write a task...')
+    alert("Write a task...");
   }
-}
+};
 
 let updateStats = () => {
-  let element = list.querySelectorAll('div')
-  let checkbox = list.querySelectorAll('input[type="checkbox"]:checked')
-  stats.innerHTML = `<p>Pending: ${element.length} Completed: ${checkbox.length}</p>`
-}
-list.addEventListener("click", (event)=> {
+  let element = list.querySelectorAll("div");
+  let checkbox = list.querySelectorAll('input[type="checkbox"]:checked');
+  stats.innerHTML = `<p>Pending: ${element.length} Completed: ${checkbox.length}</p>`;
+};
+list.addEventListener("click", (event) => {
   if (event.srcElement.nodeName == "INPUT") {
-      updateStats()
+    updateStats();
   } else if (event.srcElement.nodeName == "IMG") {
-       deleteTask(event.srcElement.parentNode.id)
+    deleteTask(event.srcElement.parentNode.id);
   }
-})
+});
 
 function deleteTask(id) {
-  let taskToDelete = document.getElementById(id)
-  list.removeChild(taskToDelete)
-  updateStats()
+  let taskToDelete = document.getElementById(id);
+  list.removeChild(taskToDelete);
+  updateStats();
 }
